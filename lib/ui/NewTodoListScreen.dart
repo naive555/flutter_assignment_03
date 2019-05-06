b34d54483b78decebe47ed01937b6e11060583ee
naive555 <non.naive@gmail.com>
2019-05-06 21:14:53 +0700

    Fix bug at NewTodoListScreen.dart.

--- lib/ui/NewTodoListScreen.dart
+++ lib/ui/NewTodoListScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-int _id = 0;
-
 class NewTodoListScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -14,6 +12,8 @@ class NewTodoListState extends State<NewTodoListScreen>{
   final _formkey = GlobalKey<FormState>();
   final textController = TextEditingController();
   Firestore _store = Firestore.instance;
+  int _id = 0;
+  int _maxId = 0;
 
    @override
   Widget build(BuildContext context) {
@@ -40,9 +40,29 @@ class NewTodoListState extends State<NewTodoListScreen>{
             RaisedButton(
               child: Text('Save'),
               onPressed: () async {
-                _id += 1;
                 _formkey.currentState.validate();
                 if (textController.text.length > 0) {
+                  QuerySnapshot getDoc = await _store.collection('todo').getDocuments();
+                  List<DocumentSnapshot> document = getDoc.documents;
+                  int docCount = document.length;
+                  _id += 1;
+                  if (docCount > 0){
+                    if (_id > 1){
+                      getDoc.documents.forEach((d){
+                        if (d.data['_id'] > _maxId){
+                          _maxId = d.data['_id'];
+                        }
+                        _id = _maxId + 1;
+                      });
+                    } else {
+                      getDoc.documents.forEach((d){
+                        if (d.data['_id'] > _maxId){
+                          _maxId = d.data['_id'];
+                        }
+                        _id = _maxId + 1;
+                      });
+                    }
+                  }
                   await _store.collection('todo').document(_id.toString()).setData({
                     '_id': _id,
                     'title': textController.text,
